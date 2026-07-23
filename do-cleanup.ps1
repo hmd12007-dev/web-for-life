0e1c9ab17f7a28fdf34f227dfbcc888df3328a31
hmd12007-dev <s.hmd12007@gmail.com>
2026-07-23 11:16:20 -0700

    Remove deployment helper scripts (cleanup)

---
+++ do-cleanup.ps1
@@ -0,0 +1,5 @@
+Set-Location 'E:\coding site\SMIT Class\Assingments\medical web\medicore-hospital'
+git add -A
+git commit -m "Remove deployment helper scripts (cleanup)"
+git push origin main
+Write-Host "Cleanup pushed!" -ForegroundColor Green
